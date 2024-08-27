15ae1b684ecfbe3cd3e7f2525107467756004be6
Hipolito <167230845+bielhipolito@users.noreply.github.com>
2024-08-27 14:06:25 -0300

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,7 @@
+def soma(a, b):
+  return a + b
+if __name__ == "__main__":
+  num1 = float(input("digite seu primeiro numero: "))
+  num2 = float(input("digite seu segundo numero: "))
+  resultado = soma(num1, num2)
+  print(f"Resultado: {resultado}")
